7e7e77cd7718506698e4c18f87006eea8ed5453b
dfeinzimer <dfeinzimer@gmail.com>
2017-02-28 12:45:02 -0800

    Begin defining flow

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#Main.cpp
+#main.cpp
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+const string AUTHOR = "David Feinzimer";
+
 int main() {
+	cout << "Welcome to Data Analysis by " << AUTHOR << endl;
+	cout << "This program will compute certain statistical values." << endl << endl;
+	while(true) {
+		cout << "Please enter floating point data." << endl;
+		cout << "After the last number has been entered press Cntl+D" << endl;
+		exit;
+	}
 	return 0;
 }
