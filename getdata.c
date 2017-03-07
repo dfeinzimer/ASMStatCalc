42c6e3c33361c1adcf04a174faa8401f81bc393e
dfeinzimer <dfeinzimer@gmail.com>
2017-03-07 10:47:42 -0800

    getdata() implementation

--- getdata.c
+++ getdata.c
@@ -1,1 +1,14 @@
 #getdata.c
+
+using namespace std;
+
+#include <float.h>
+#include <stdio.h> 
+#include <vector>
+
+vector<int> getdata() {
+	vector<double> data;
+	printf("Please enter floating point data.\n");
+	printf("After the last number has been entered press Cntl+D\n");
+	return data;
+}
