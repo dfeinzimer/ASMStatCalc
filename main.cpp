b8ad41e5288db010dafe9b00497b55fac2e3b1d9
dfeinzimer <dfeinzimer@gmail.com>
2017-03-07 09:28:01 -0800

    Modify main

--- main.cpp
+++ main.cpp
@@ -2,18 +2,18 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include "getdata.c"
 
 using namespace std;
 
 const string AUTHOR = "David Feinzimer";
 
 int main() {
+	vector<double> data;
 	cout << "Welcome to Data Analysis by " << AUTHOR << endl;
 	cout << "This program will compute certain statistical values." << endl << endl;
-	while(true) {
-		cout << "Please enter floating point data." << endl;
-		cout << "After the last number has been entered press Cntl+D" << endl;
-		exit;
-	}
+	data = getdata();
 	return 0;
 }
