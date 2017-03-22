4b936b7ad183df436a3034aaaa96a21479a6b4ed
David Feinzimer <dfeinzimer@gmail.com>
2017-03-22 00:15:10 -0700

    Add new shell file rename old

---
+++ p3.sh
@@ -0,0 +1,30 @@
+#Author: David Feinzimer
+#Course: CPSC254
+
+echo Script file for project 3 begins
+
+#remove old objects
+rm *.o
+rm *.out
+
+#compile cpp files
+g++ -m64 -c -o computemean.o computemean.cpp
+g++ -m64 -c -o computemedian.o computemedian.cpp
+g++ -m64 -c -o harmonicmean.o harmonicmean.cpp
+g++ -m64 -c -o main.o main.cpp
+
+#compile c files
+gcc -m64 -c -o display.o display.c
+gcc -m64 -c -o getdata.o getdata.c
+gcc -m64 -c -o sort.o sort.c
+
+#list all
+ls -l
+
+#link all files
+g++ -m64 -o project.out computemean.o computemedian.o harmonicmean.o main.o display.o getdata.o sort.o
+
+#call program
+./project.out
+
+echo good-bye
